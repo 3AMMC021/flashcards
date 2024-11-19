c0124c9c4f6b9379589dc395f530ca1b05f0cd0b
3AMMC021 <00001092269009sp@al.educacao.sp.gov.br>
2024-11-19 09:57:55 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Anatomia',
+    'Qual é a principal função do fígado?',
+    'Umas das suas principais funções é a filtragem do sangue e a eliminação das toxinas'
+)
+
+criaCartao(
+    'Astronomia',
+    'Quais são as fases da lua?',
+    'Nova, crescente, cheia e minguante'
+)
+
+criaCartao(
+    'Ciência',
+    'Qual cientista descobriu o processo de pasteurização e a vacina contra a raiva?',
+    'Louis Pasteur'
+)
+
+criaCartao(
+    'Biologia',
+    'Como se chamam os vasos que transportam sangue do coração para a periferia do corpo?',
+    'Artérias'
+)
+
+criaCartao (
+    'História',
+    'Em que século o continente europeu foi devastado pela peste bubônica?',
+    'Século XIV'
+)
+
+criaCartao (
+    'Filosofia',
+    'De quem é a famosa frase “Penso, logo existo”?',
+    'Decartes'
+)
